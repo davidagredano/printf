bf05d10e5f94ef8eb1288462e6e7253c5d8ec37e
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-08 16:33:09 +0100

    Initial implementation of ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 10:14:15 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/08 15:15:18 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/08 16:27:15 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,23 @@
 
 int	ft_printf(char const *format, ...)
 {
-	char *str;
+	char 	*str;
+	va_list	ap;
+	size_t	i;
 
 	str = (char *) ft_calloc(BLOCK_SIZE, sizeof(char));
 
-	ft_strlcpy(str, format, BLOCK_SIZE);
-	ft_putstr_fd(str, STDOUT_FILENO);
+	va_start(ap, format);
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+			ft_putstr_fd(va_arg(ap, char *), STDOUT_FILENO); //TODO
+		else
+			ft_putchar_fd(format[i], STDOUT_FILENO);
+		i++;
+	}
 	free(str);
+	va_end(ap);
 	return (1);
 }
