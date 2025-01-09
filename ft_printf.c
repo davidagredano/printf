de5817d11a5483624f60315f7c24d33d2da37706
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-09 18:54:31 +0100

    Add format distribution in ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 10:14:15 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/08 16:27:15 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/09 18:50:10 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,25 @@ int	ft_printf(char const *format, ...)
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
-			ft_putstr_fd(va_arg(ap, char *), STDOUT_FILENO); //TODO
+		{
+			i++;
+			if (format[i] == 'd' || format[i] == 'i')
+				ft_print_int(va_arg(ap, int));
+			else if (format[i] == 'u')
+				ft_print_uint(va_arg(ap, unsigned int));
+			else if (format[i] == 'x')
+				ft_print_hex(va_arg(ap, unsigned int), false);
+			else if (format[i] == 'X')
+				ft_print_hex(va_arg(ap, unsigned int), true);
+			else if (format[i] == 'c')
+				ft_print_char(va_arg(ap, int));
+			else if (format[i] == 's')
+				ft_print_str(va_arg(ap, const char *));
+			else if (format[i] == 'p')
+				ft_print_ptr(va_arg(ap, void *));
+			else if (format[i] == '%')
+				ft_print_percent();
+		}
 		else
 			ft_putchar_fd(format[i], STDOUT_FILENO);
 		i++;
