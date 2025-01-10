1086064c7a541004b95d4a434c11232f5a4e4853
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-10 10:26:19 +0100

    Extract format distribution logic in a function

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,40 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 10:14:15 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/09 18:50:10 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/10 10:24:52 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h" 
 
+static int	ft_print_format(char specifier, va_list ap)
+{
+	if (specifier == 'd' || specifier == 'i')
+		return (ft_print_int(va_arg(ap, int)));
+	else if (specifier == 'u')
+		return (ft_print_uint(va_arg(ap, unsigned int)));
+	else if (specifier == 'x')
+		return (ft_print_hex(va_arg(ap, unsigned int), false));
+	else if (specifier == 'X')
+		return (ft_print_hex(va_arg(ap, unsigned int), true));
+	else if (specifier == 'c')
+		return (ft_print_char(va_arg(ap, int)));
+	else if (specifier == 's')
+		return (ft_print_str(va_arg(ap, const char *)));
+	else if (specifier == 'p')
+		return (ft_print_ptr(va_arg(ap, void *)));
+	else if (specifier == '%')
+		return (ft_print_percent());
+	return (0);
+}
+
 int	ft_printf(char const *format, ...)
 {
-	char 	*str;
+	char	*str;
 	va_list	ap;
 	size_t	i;
 
 	str = (char *) ft_calloc(BLOCK_SIZE, sizeof(char));
-
 	va_start(ap, format);
 	i = 0;
 	while (format[i] != '\0')
@@ -27,22 +47,7 @@ int	ft_printf(char const *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'd' || format[i] == 'i')
-				ft_print_int(va_arg(ap, int));
-			else if (format[i] == 'u')
-				ft_print_uint(va_arg(ap, unsigned int));
-			else if (format[i] == 'x')
-				ft_print_hex(va_arg(ap, unsigned int), false);
-			else if (format[i] == 'X')
-				ft_print_hex(va_arg(ap, unsigned int), true);
-			else if (format[i] == 'c')
-				ft_print_char(va_arg(ap, int));
-			else if (format[i] == 's')
-				ft_print_str(va_arg(ap, const char *));
-			else if (format[i] == 'p')
-				ft_print_ptr(va_arg(ap, void *));
-			else if (format[i] == '%')
-				ft_print_percent();
+			ft_print_format(format[i], ap);
 		}
 		else
 			ft_putchar_fd(format[i], STDOUT_FILENO);
