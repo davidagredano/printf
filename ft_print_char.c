a03372c53c93d14dabf8fe4cea9ce5cd4cb12e6b
David Agredano <davidagredano@gmail.com>
2025-01-12 16:02:00 +0100

    Cleanup ft_print_char

--- ft_print_char.c
+++ ft_print_char.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:49:45 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/10 14:13:08 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/12 15:57:00 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,5 @@
 
 int	ft_print_char(int c)
 {
-	// print the char
-	ft_putchar_fd((unsigned char) c, 1);
-
-	// return the number of chars printed (always 1)
-	return (1);
+	return ((int) write(STDOUT_FILENO, &c, 1));
 }
