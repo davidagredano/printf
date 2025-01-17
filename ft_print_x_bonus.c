f54cc54c11f01fe6604e631aa0a42f42f84e518f
David Agredano <davidagredano@gmail.com>
2025-01-17 20:17:25 +0100

    Update ft_print_x_bonus to produce the correct output when the input is 0

--- ft_print_x_bonus.c
+++ ft_print_x_bonus.c
@@ -6,17 +6,17 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 11:59:51 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/17 17:00:22 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/17 19:38:28 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_validate_spec(char *digits, t_spec *spec);
+static void	ft_validate_spec(char *conv, t_spec *spec);
 static char	*ft_get_base_str(t_spec *spec);
-static void	ft_insert_digits(char *dst, const char *src, t_spec *spec);
-static void	ft_insert_digits_ltr(char *dst, const char *src, t_spec *spec);
-static void	ft_insert_digits_rtl(char *dst, const char *src, t_spec *spec);
+static void	ft_insert_conv(char *dst, const char *src, t_spec *spec);
+static void	ft_insert_conv_ltr(char *dst, const char *src, t_spec *spec);
+static void	ft_insert_conv_rtl(char *dst, const char *src, t_spec *spec);
 
 /**
  * The unsigned int argument is converted to unsigned hexadecimal notation.
@@ -25,49 +25,55 @@ static void	ft_insert_digits_rtl(char *dst, const char *src, t_spec *spec);
  * The precision, if any, gives the minimum number of digits that must appear;
  * if the converted value requires fewer digits, it is padded on the left
  * with zeros.
+ * When 0 is printed with an explicit precision 0, the output is empty.
  * Flags admitted: alternative form, zero padding, left justification,
  * field width, precision.
  */
 int	ft_print_x_bonus(unsigned n, t_spec *spec)
 {
 	char	*str;
-	char	*digits;
+	char	*conv;
 	int		chars_printed;
 
-	if (spec->specifier == 'x')
-		digits = ft_uitoa(n, 16);
-	if (spec->specifier == 'X')
-		digits = ft_uitoa_caps(n, 16);
-	if (!digits)
+	if (n == 0 && spec->precision == 0)
+		conv = ft_strdup("");
+	else if (spec->specifier == 'x')
+		conv = ft_uitoa(n, 16);
+	else
+		conv = ft_uitoa_caps(n, 16);
+	if (!conv)
 		return (0);
-	ft_validate_spec(digits, spec);
+	ft_validate_spec(conv, spec);
 	str = ft_get_base_str(spec);
 	if (!str)
 	{
-		free(digits);
+		free(conv);
 		return (0);
 	}
-	ft_insert_digits(str, digits, spec);
+	ft_insert_conv(str, conv, spec);
 	chars_printed = ft_putstr(str);
-	free(digits);
+	free(conv);
 	free(str);
 	return (chars_printed);
 }
 
 /**
+ * Ignores '#' if the conversion is "0" or "", which means that the input was 0.
  * Ignores '0' flag if left justification or precision is given.
  * Resets the unsupported ' ' and '+' flags.
  * Ensures a minimum precision and field width to avoid truncation,
  * adding extra space if '#' flag is present.
  */
-static void	ft_validate_spec(char *digits, t_spec *spec)
+static void	ft_validate_spec(char *conv, t_spec *spec)
 {
+	if (ft_strncmp(conv, "0", 2) == 0 || ft_strncmp(conv, "", 1) == 0)
+		spec->alternative_form = false;
 	if (spec->left_align || spec->precision >= 0)
 		spec->zero_padding = false;
 	spec->leading_blank = false;
 	spec->leading_sign = '\0';
-	if (spec->precision < (int) ft_strlen(digits))
-		spec->precision = ft_strlen(digits);
+	if (spec->precision < (int) ft_strlen(conv))
+		spec->precision = ft_strlen(conv);
 	if (spec->alternative_form)
 		spec->precision += 2;
 	if (spec->field_width < spec->precision)
@@ -97,12 +103,12 @@ static char	*ft_get_base_str(t_spec *spec)
 /**
  * Calls one of the insertion functions depending on the left alignment flag.
  */
-static void	ft_insert_digits(char *dst, const char *src, t_spec *spec)
+static void	ft_insert_conv(char *dst, const char *src, t_spec *spec)
 {
 	if (spec->left_align)
-		ft_insert_digits_ltr(dst, src, spec);
+		ft_insert_conv_ltr(dst, src, spec);
 	else
-		ft_insert_digits_rtl(dst, src, spec);
+		ft_insert_conv_rtl(dst, src, spec);
 }
 
 /**
@@ -111,7 +117,7 @@ static void	ft_insert_digits(char *dst, const char *src, t_spec *spec)
  * is present, and adds '0' characters if there is room to fill in
  * the precision size before copying the src.
  */
-static void	ft_insert_digits_ltr(char *dst, const char *src, t_spec *spec)
+static void	ft_insert_conv_ltr(char *dst, const char *src, t_spec *spec)
 {
 	int	i;
 	int	lead_signs;
@@ -137,7 +143,7 @@ static void	ft_insert_digits_ltr(char *dst, const char *src, t_spec *spec)
  * is present, and adds '0' characters if there is room to fill in
  * the precision size before copying the src.
  */
-static void	ft_insert_digits_rtl(char *dst, const char *src, t_spec *spec)
+static void	ft_insert_conv_rtl(char *dst, const char *src, t_spec *spec)
 {
 	int	i;
 	int	padding;
