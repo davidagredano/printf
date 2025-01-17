614fbbc3321542fb7e9329e60ed5c80ae6c1049f
David Agredano <davidagredano@gmail.com>
2025-01-17 19:04:06 +0100

    Update ft_print_c_bonus to fix a couple bugs

--- ft_print_c_bonus.c
+++ ft_print_c_bonus.c
@@ -6,12 +6,13 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:49:45 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/14 16:56:18 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/17 19:02:46 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void	ft_validate_spec(int c, t_spec *spec);
 static char	*ft_get_base_str(t_spec *spec);
 static void	ft_insert_char(int c, char *str, t_spec *spec);
 
@@ -23,37 +24,53 @@ static void	ft_insert_char(int c, char *str, t_spec *spec);
 int	ft_print_c_bonus(int c, t_spec *spec)
 {
 	char	*str;
-	int		chars_printed;
 
+	ft_validate_spec(c, spec);
 	str = ft_get_base_str(spec);
 	if (!str)
 		return (0);
 	ft_insert_char(c, str, spec);
-	chars_printed = ft_putstr(str);
+	ft_putstr(str);
 	free(str);
-	return (chars_printed);
+	return (spec->field_width);
 }
 
 /**
- * Allocates a string of the appropriate size.
- * Initializes all characters to ' ' except for the terminating NUL character.
- * Returns a pointer to the allocated string, or NULL if allocation fails.
+ * Ensures that NUL character won't be inserted at the beginning of the str if the
+ * left align flag is present. That would print an empty string as a result.
+ * Ensures a minimum field width of 1 to copy the character.
+ * Resets the rest of the unsupported flags.
  */
+static void	ft_validate_spec(int c, t_spec *spec)
+{
+	spec->alternative_form = false;
+	spec->zero_padding = false;
+	if (c == '\0')
+		spec->left_align = false;
+	spec->leading_blank = false;
+	spec->leading_sign = '\0';
+	if (spec->field_width < 1)
+		spec->field_width = 1;
+	spec->precision = -1;
+}
+
 static char	*ft_get_base_str(t_spec *spec)
 {
 	char	*str;
 
-	if (spec->field_width < 1)
-		spec->field_width = 1;
 	str = ft_calloc(spec->field_width + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_memset(str, ' ', spec->field_width);
+	if (spec->zero_padding)
+		ft_memset(str, '0', spec->field_width);
+	else
+		ft_memset(str, ' ', spec->field_width);
 	return (str);
 }
 
 /**
- * Inserts the character to the correct position in the string.
+ * Copies the character at the start or the end of the buffer pointed to by dst,
+ * depending if the left align flag is present or not.
  */
 static void	ft_insert_char(int c, char *dest, t_spec *spec)
 {
