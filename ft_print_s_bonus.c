a05bcf9b12a85014b6ae39cde1b42db303fc537e
David Agredano <davidagredano@gmail.com>
2025-01-20 00:19:23 +0100

    Complete and unify spec validations for print s

--- ft_print_s_bonus.c
+++ ft_print_s_bonus.c
@@ -6,14 +6,13 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:18:11 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/18 20:43:57 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/20 00:18:49 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void	ft_validate_precision(const char *s, t_spec *spec);
-static void	ft_validate_field_width(t_spec *spec);
+static void	ft_validate_spec(const char *s, t_spec *spec);
 static char	*ft_get_base_str(t_spec *spec);
 static void	ft_insert_str(char *dst, const char *src, t_spec *spec);
 
@@ -31,8 +30,7 @@ int	ft_print_s_bonus(const char *s, t_spec *spec)
 		s = "";
 	else if (!s)
 		s = "(null)";
-	ft_validate_precision(s, spec);
-	ft_validate_field_width(spec);
+	ft_validate_spec(s, spec);
 	str = ft_get_base_str(spec);
 	if (!str)
 		return (-1);
@@ -43,32 +41,31 @@ int	ft_print_s_bonus(const char *s, t_spec *spec)
 }
 
 /**
- * To comply with the spec:
- * If a precision is given, no NUL byte needs to be present;
- * if the precision is not specified, or is greater than the size of the string,
- * the string must contain a terminating NUL byte
+ * Ignores not used flags for this spec.
+ * If a precision is given, no NUL byte needs to be present; if the precision
+ * is not specified, or is greater than the size of the string, the string must
+ * contain a terminating NUL byte.
+ * Ensures a minimum precision and field width to avoid truncation.
  */
-static void	ft_validate_precision(const char *s, t_spec *spec)
+static void	ft_validate_spec(const char *s, t_spec *spec)
 {
+	spec->alternative_form = false;
+	spec->zero_padding = false;
+	spec->leading_blank = false;
+	spec->leading_sign = false;
 	if (spec->precision >= 0)
 		spec->precision = ft_strnlen(s, spec->precision);
 	else
 		spec->precision = ft_strlen(s);
-}
 
-/**
- * Ensures that the field width won't cause truncation of the resulting string,
- * expanding its size to the number of characters that will be written.
- */
-static void	ft_validate_field_width(t_spec *spec)
-{
 	if (spec->field_width < spec->precision)
 		spec->field_width = spec->precision;
 }
 
 /**
  * Allocates a string of the appropriate size.
- * Initializes all characters to ' ' except for the terminating NUL character.
+ * Initializes all characters except for the terminating NUL character to ' ',
+ * or '0' if the zero_padding flag is present.
  * Returns a pointer to the allocated string, or NULL if allocation fails.
  */
 static char	*ft_get_base_str(t_spec *spec)
@@ -78,7 +75,10 @@ static char	*ft_get_base_str(t_spec *spec)
 	str = ft_calloc(spec->field_width + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_memset(str, ' ', spec->field_width);
+	if (spec->zero_padding)
+		ft_memset(str, '0', spec->field_width);
+	else
+		ft_memset(str, ' ', spec->field_width);
 	return (str);
 }
 
